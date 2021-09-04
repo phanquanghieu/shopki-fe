9e484b741d84f74c86a1eb5c74a881bc301742f0
chinhcd <7a3dqh@gmail.com>
2021-09-04 23:39:56 +0700

    d

--- src/layouts/SellerLayout/SellerLayout.jsx
+++ src/layouts/SellerLayout/SellerLayout.jsx
@@ -1,7 +1,11 @@
 import React, { useState, useEffect } from 'react'
 
 function SellerLayout(props) {
-  return <div>seller</div>
+  return (
+    <div>
+      day la seller
+    </div>
+  )
 }
 
 export default SellerLayout
