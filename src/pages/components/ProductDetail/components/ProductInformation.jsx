98ac90f79fb5ddb00ca86b241d43e4ae7079db3e
chinhcd <7a3dqh@gmail.com>
2021-10-20 17:12:59 +0700

    d

---
+++ src/pages/components/ProductDetail/components/ProductInformation.jsx
@@ -0,0 +1,28 @@
+import '../style/productInformationStyle.scss'
+
+function ProductInformation(props){
+  const handleAddToCart=(id)=>{
+    console.log(id)
+  }
+  return(
+    <div id="product-detail" className="container d-flex">
+      <div className="image-product">
+
+      </div>
+      <div className="product-information">
+        <div className="name-product">
+          <h1>Chinh ahihi</h1>
+        </div>
+        <div className="price">
+          <p>500.000 đ</p>
+        </div>
+        <div className="add-to-cart">
+          <div className="position-absolute add-to-cart" onClick={()=>handleAddToCart(props.product.id)}>
+            <i className="fas fa-cart-plus"></i>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+export default ProductInformation;
--- src/pages/components/ProductDetail/index.jsx
+++ src/pages/components/ProductDetail/index.jsx
@@ -1,13 +1,28 @@
-import { useEffect } from 'react'
+import { useEffect, useState } from 'react'
+import Header from '../../Seller/HomeSeller/components/header'
+import BuyerLayoutHeader from '../../../layouts/BuyerLayout/components/BuyerLayoutHeader'
+import ProductInformation from './components/ProductInformation'
+import request from '../../../services/request'
 
-function ProductDetail(props){
-  useEffect(()=>{
+function ProductDetail(props) {
+  useEffect(() => {
     console.log(props.match.params.id)
+
   })
-  return(
+  const [product,setProduct]=useState([]);
+  const getProduct=async ()=>{
+    let res = await request.get('/api/product');
+    if (res){
+      console.log(res)
+      setProduct(res.products);
+    }
+  }
+  return (
     <div>
-day la trang detail
+      <BuyerLayoutHeader />
+      <ProductInformation />
     </div>
   )
 }
-export default ProductDetail;
+
+export default ProductDetail
